0a24e88722e7df35b2ded3280c8a0755fb85c5e9
cainantm <cainantm@hotmail.com>
2022-03-16 16:54:50 -0300

    Área do quadrilátero usando return

--- src/dio/bootcamp/Main.java
+++ src/dio/bootcamp/Main.java
@@ -4,12 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Quadrilatero
+        //Utilizado para mostrar os resultados em sobrecarga
+     /*   //Quadrilatero
         System.out.println("Exercício quadrilatero:");
         Quadrilatero.area(3);
         Quadrilatero.area(5,5);
-        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(7,8,9);*/
 
+        //Utilizado para os resultados em return
+
+        System.out.println("Exercício Retornos: ");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5,5);
+        System.out.println("Área do retangulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("Área do trapezio: " + areaTrapezio);
 
     }
 }
--- src/dio/bootcamp/Quadrilatero.java
+++ src/dio/bootcamp/Quadrilatero.java
@@ -2,7 +2,10 @@ package dio.bootcamp;
 
 public class Quadrilatero {
 
-    public static void area(double lado){
+    //Fazendo com método de sobrecarga
+
+    /*public static void area(double lado){
+
         System.out.println("Área do quadrado: " + lado * lado);
     }
 
@@ -13,5 +16,19 @@ public class Quadrilatero {
 
     public static void area(double baseMaior, double baseMenor, double altura){
         System.out.println("Área do trapézio: " + ((baseMaior+baseMenor)*altura) / 2);
+    }*/
+
+    //fazendo com método return
+
+    public static double area(double lado){
+        return lado * lado;
+    }
+
+    public static double area(double lado1, double lado2){
+        return lado1 * lado2;
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura){
+        return ((baseMaior+baseMenor)*altura)/2;
     }
 }
